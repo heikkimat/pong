2993de2a61cfa779cbb3beec13d67c91f30cd778
heikkimat <heikkimat@gmail.com>
2021-03-14 17:56:59 +0200

    Add files via upload

--- pong.js
+++ pong.js
@@ -8,7 +8,7 @@ class Vector {
 
 class Rectangular {
   constructor(w, h) {
-    this.pos = new Vector;
+    this.pos = new Vector; // keskikohta
     this.size = new Vector(w, h);
   }
   get left() {
@@ -23,12 +23,23 @@ class Rectangular {
   get bottom() {
     return this.pos.y + this.size.y / 2;
   }
+  
 }
 
 class Ball extends Rectangular {
   constructor() {
     super(10, 10);
-    this.velocity = new Vector;
+	this.velocity = new Vector;
+    this.vel = 300;
+	this.angle = Math.PI / 4 * (Math.random() > .5 ? 1 : -1); //+/-45 deg
+	this.alive = true;
+  }
+  
+  get velocity_x() {
+	return this.vel * Math.cos(this.angle);
+  }
+  get velocity_y() {
+	return this.vel * Math.sin(this.angle);
   }
 }
 
@@ -36,6 +47,8 @@ class Player extends Rectangular {
   constructor() {
     super(20, 100);
     this.score = 0;
+	this.velocity = -50;
+	this.visible = false;
   }
 }
 
@@ -46,23 +59,21 @@ class Pong {
 
     this.ball = new Ball;
 
-    this.ball.pos.x = 100;
-    this.ball.pos.y = 100;
-    this.ball.velocity.x = 100;
-    this.ball.velocity.y = 100;
-
     this.players = [
       new Player,
-      new Player
+      new Player,
+	  new Player
     ];
 
     this.players[0].pos.x = 40;
     this.players[1].pos.x = this.canvas.width - 40;
+    this.players[2].pos.x = this.canvas.width / 2;
     this.players.forEach(player => {
       player.pos.y = this.canvas.height / 2;
     });
+	this.players[2].pos.y = this.canvas.height + 50;
 
-    let lastTime;
+    let lastTime; // previous page update
     const callback = (millis) => {
       if (lastTime) {
         this.update((millis -lastTime)/1000);
@@ -74,25 +85,42 @@ class Pong {
     callback();
   }
 
-  /* collide(player, ball) {
-    if (ball.left < player.right && ball.right > player.left && 
-      ball.bottom > player.top && ball.top < player.bottom) {
-      ball.velocity.x = - ball.velocity.x * 1.05;
-    }
-  } */
   collide(ball) {
+	// player 0, mouse
     if (ball.velocity.x < 0 && ball.left < this.players[0].right && 
-        ball.top > this.players[0].top && ball.bottom < this.players[0].bottom) {
-      ball.velocity.x = - ball.velocity.x * 1.1;
-    } else if (ball.velocity.x < 0 && ball.right > this.players[0].left && 
-              ball.left < this.players[0].right &&
-              ball.bottom > this.players[0].top && ball.top < this.players[0].bottom) {
-      ball.velocity.y = - ball.velocity.y;
+        ball.top > this.players[0].top && ball.bottom < this.players[0].bottom && ball.alive) {
+      ball.vel = Math.min(ball.vel * 1.1, 700)
+	  ball.angle = -Math.PI * 1/4 + Math.PI / 2 * Math.random();
+	  ball.velocity.x = ball.velocity_x;
+	  ball.velocity.y = ball.velocity_y;
+	  this.players[0].score++
+	  document.getElementById("score").innerHTML = this.players[0].score;
+	  // blocker visibility
+	  if (this.players[0].score >= 30) {
+		  this.players[2].visible = true;
+	  }
+    } 
+	// if pass the bat
+	else if (ball.velocity.x < 0 && ball.left < this.players[0].right) {
+      ball.alive = false
     }
+	
+	// player 1, computer
     if (ball.velocity.x > 0 && ball.right > this.players[1].left && 
         ball.top > this.players[1].top && ball.bottom < this.players[1].bottom) {
-    ball.velocity.x = - ball.velocity.x * 1.1;
-    } 
+      
+	  ball.vel = Math.min(ball.vel * 1.1, 700);
+	  ball.angle = Math.PI * 3/4 + Math.PI / 2 * Math.random();
+	  ball.velocity.x = ball.velocity_x;
+	  ball.velocity.y = ball.velocity_y;
+	}
+	
+	//player 2, blocker
+	if(this.players[2].visible && ball.left < this.players[2].right && ball.right > this.players[2].left && 
+        ball.top > this.players[2].top && ball.bottom < this.players[2].bottom) {
+	  ball.velocity.x = -ball.velocity.x;
+	}
+	
   }
 
   draw() {
@@ -100,7 +128,12 @@ class Pong {
     this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
     this.drawRect(this.ball);
-    this.players.forEach(player => this.drawRect(player));
+    //this.players.forEach(player => this.drawRect(player));
+	this.drawRect(this.players[0]);
+	this.drawRect(this.players[1]);
+	if (this.players[2].visible) {
+		this.drawRect(this.players[2]);
+	}
   }
 
   drawRect(rect) {
@@ -108,27 +141,46 @@ class Pong {
     this.context.fillRect(rect.left, rect.top, 
                           rect.size.x, rect.size.y);
   }
+  
+  reset() {
+    this.ball.pos.x = 200;
+    this.ball.pos.y = canvas.height * (Math.random()); // Y axis position, 0 to canvas height;
+    this.ball.vel = Math.max(this.ball.vel / 2, 50);
+	this.ball.angle = Math.PI / 4 * (Math.random() > .5 ? 1 : -1); //+/-45 deg
+	this.ball.velocity.x = this.ball.velocity_x;
+    this.ball.velocity.y = this.ball.velocity_y;
+	this.ball.alive = true;
+  };
 
   update(dt) {
     this.ball.pos.x += this.ball.velocity.x *dt;
     this.ball.pos.y += this.ball.velocity.y *dt;
-  
-    if (this.ball.left < 0 || this.ball.right > this.canvas.width) {
-      this.ball.velocity.x = -this.ball.velocity.x;
-    }
-    if (this.ball.top < 0 || this.ball.bottom > this.canvas.height) {
+	// blocker
+	if(this.players[2].visible) {
+	  this.players[2].pos.y += this.players[2].velocity *dt;
+	}
+    
+	if (this.ball.top < 0 || this.ball.bottom > this.canvas.height) {
       this.ball.velocity.y = -this.ball.velocity.y;
+	  this.ball.pos.y += this.ball.velocity.y *dt;
     }
 
-    //this.players[0].pos.y = this.ball.pos.y;
     this.players[1].pos.y = this.ball.pos.y;
-
-    //this.players.forEach(player => this.collide(player, this.ball));
+	
+	if (this.players[2].pos.y < -50 || this.players[2].pos.y > this.canvas.height + 50) {
+		this.players[2].velocity = -this.players[2].velocity
+	}
+	
     this.collide(this.ball);
+	
+	// Detect if Ball touches bounds of Canvas
+    if (this.ball.left < 0) {
+      this.reset();
+    }
   
     this.draw();
   }
-  
+
 }
 
 
